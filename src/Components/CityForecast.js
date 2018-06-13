88e8d5a7618bea4dd99b595c4390c75a34e3bbb0
mthorry <mthorry@gmail.com>
2018-06-12 21:15:05 -0400

    forecasts rendering

--- src/Components/CityForecast.js
+++ src/Components/CityForecast.js
@@ -22,6 +22,27 @@ class CityForecast extends Component {
     })
   }
 
+  timeModifier = (time) => {
+    if (time > 12) {
+      return `${time-12} pm`
+    } else {
+      return `${time} am`
+    }
+  }
+
+  renderForecastItem = () => {
+    console.log('renderForecastItem')
+    return this.state.forecast.map( forecast => {
+      let d = new Date(`${forecast.dt_txt}`)
+      return <div key={forecast.dt_txt} className='forecast-item'>
+        <p><strong>{d.getMonth()+1}/{d.getUTCDate()} at {this.timeModifier(d.getUTCHours())}:</strong> {Math.round(forecast.main.temp)}˚F and {forecast.weather[0].main}</p>
+        <p>{forecast.weather[0].description}</p>
+        <img src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} alt={forecast.weather[0].description} />
+        <br/>
+        </div>
+    })
+  }
+
   render() {
     console.log("CityForecast: ", this.state)
     const { forecast, city } = this.state
@@ -29,6 +50,10 @@ class CityForecast extends Component {
     return (
       <div className="CityForecast">
         <h2> Forecast for {city.name}</h2>
+        <br/>
+        <div className='forecast-container'>
+          {this.state.city.name ? this.renderForecastItem() : null}
+        </div>
       </div>
     );
   }
